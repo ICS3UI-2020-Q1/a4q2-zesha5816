6fe4b47e396b56d84b306c20113f6636dd1d1de7
zesha5816 <zesha5816@wrdsb.ca>
2020-10-02 15:00:25 +0000

    finished the example

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Write a program that will prompt the user to enter in a positive integer. It will keep  * doing this until the user enters in a negative integer
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,16 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    /// Create the Scanner
+		Scanner input = new Scanner(System.in);
+		//initializing a variable 
+		int count = 0;
+		int number;
+		//if the variable isnt a negative it keeps running
+		do {
+		System.out.println("Please enter a positive integer. Enter a negative integer to quit.");
+		number =input.nextInt();
+		}while (number >= count );
+		System.out.println("All done!");
   }
 }
